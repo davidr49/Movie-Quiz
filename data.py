04bf0dbc483deed396ec63a7e24b0b29a27ddb79
davidr3489@gmail.com <crowden3489>
2022-04-12 11:23:31 +0200

    Python Movie Quiz

---
+++ data.py
@@ -0,0 +1,33 @@
+question_data = [
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "The movie The Nightmare before Christmas was all done with physical objects.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Leonardo DiCaprio won an Oscar for Best Actor in The Aviator",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Matt Damon played an astronaut stranded on an extraterrestrial planet in both of the movies "
+                 "Interstellar and The Martian.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "The word Inception came from the 2010 blockbuster hit Inception.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Samuel L. Jackson had the words, Bad Motherf*cker in-scripted on his lightsaber during the filming "
+                 "of Star Wars.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "In Alfred Hitchcock's film Psycho it is said he used chocolate syrup to simulate the blood in the "
+                 "famous shower scene.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "medium",
+     "question": "Sean Connery wasn't in Indiana Jones and the Kingdom of the Crystal Skull because he found "
+                 "retirement too enjoyable.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "George Lucas directed the entire original Star Wars trilogy.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Entertainment: Film", "type": "boolean", "difficulty": "medium",
+                                      "question": "Joan Cusack starred in the 2009 disaster movie, 2012.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Film", "type": "boolean", "difficulty": "easy",
+     "question": "Actor Tommy Chong served prison time.", "correct_answer": "True", "incorrect_answers": ["False"]}]
